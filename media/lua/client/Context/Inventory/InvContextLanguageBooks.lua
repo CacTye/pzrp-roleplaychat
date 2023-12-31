614613d755539b7779d3e72927485b35de15a583
Enzo  Carbone <devotan.contact@gmail.com>
2023-12-31 13:13:51 -0600

    Content Changes

--- media/lua/client/Context/Inventory/InvContextLanguageBooks.lua
+++ media/lua/client/Context/Inventory/InvContextLanguageBooks.lua
@@ -7,7 +7,7 @@ function ISInventoryMenuElements.ContextLanguageBooks(context)
     self.invMenu = ISContextManager.getInstance().getInventoryMenu();
     local player = getPlayer()
     local modData = player:getModData()
-    local languages = { "Spanish", "Russian", "Ukrainian", "German", "French", "Mandarin", "Japanese", "ASL" }
+    local languages = { "Spanish", "Russian", "Ukrainian", "German", "French", "Mandarin", "Japanese", "ASL", "Cantonese", "Arabic", "Hindi", "Portugese", "Korean", "Dutch", "Polish", "Greek", "Hebrew", "Scandinavian", "Italian", "Tagalog" }
     function self.init()
     end
 
